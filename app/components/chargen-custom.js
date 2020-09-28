f8b3ec6c84c8f710eee9198d0cc511c6eea57f83
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-28 12:52:14 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -12,6 +12,9 @@ export default Component.extend({
 	}), 
 
     iconicfChanged(val) {
+		var charif;
+		charif = this.get('char.custom.chariconicf');
+		console.log (charif);
         this.set('char.custom.chariconicf', val.value);
     },
 	
