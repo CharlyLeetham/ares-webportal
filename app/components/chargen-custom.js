b36a5dcebfe64927e395284949d7295a1de6b722
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-09-29 15:33:30 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -27,7 +27,8 @@ export default Component.extend({
     // Return a hash containing your data.  Character data will be in 'char'.  For example:
     // 
     // return { goals: this.get('char.custom.goals') };
-	console.log (char.custom.iconicf)
+	console.log 'kkkk';
+	console.log (char.custom.iconicf);
 	return { iconicf: this.get('char.custom.iconicf')};
   },
   
