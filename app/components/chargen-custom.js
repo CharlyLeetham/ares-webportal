c317dd7a4813a350070636763c6ba1bd4e2a65a3
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-04 12:49:49 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -37,7 +37,7 @@ export default Component.extend({
 			this.set('char.custom.chariconicf', val)
 			charcgp = this.get('char.custom.inicgpoints');
 			console.log (charcgp)
-			for (const [key, value] of Object.entries(charcg)) {
+			for (const [key, value] of Object.entries(charcgp)) {
 				console.log(`Key: ${key}: ${value}`);
 				for (const [key1, value1] of Object.entries(value)) {
 					console.log ("inp-" + key + "=" + value1[rating])
