443b6f7ac584fe9a661ec11bb20baad07e6dadf9
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-03 19:19:30 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -43,7 +43,7 @@ export default Component.extend({
 			  //list.push({ value: g });
 			//});
 			//console.log(list);
-			charcg.forEach(element => console.log(element));	
+			console.log(typeof charcg);	
 			//get back the chargen points for the iconicframework
 			//get the current chargen points.
 			//add the if points to the current points
