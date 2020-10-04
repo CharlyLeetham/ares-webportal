157e1df3db6ec2e6f3766ee0196e0b1075373a94
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-03 18:39:49 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -44,7 +44,7 @@ export default Component.extend({
 			//});
 			console.log(list);
 			for (const [key, value] of Object.entries(list)) {
-			  console.log(`${key}: ${value}`);
+			  console.log('Key: ${key} Value: ${value}`);
 			}			
 			//get back the chargen points for the iconicframework
 			//get the current chargen points.
