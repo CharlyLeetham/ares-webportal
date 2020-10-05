6f688ed70d8b912b736c304f5edfe9612f6379ca
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-04 20:41:28 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -34,10 +34,11 @@ export default Component.extend({
 		iconicfChanged(val) {
 			var charif, charcgp, cgslots, newifpoints;
 			charif = this.get('char.custom.chariconicf');
+			console.log('charif');
 			this.set('char.custom.chariconicf', val)
 			charcgp = this.get('char.custom.inicgpoints');
 			cgslots = this.get('char.custom.cgslots');
-			newifpoints = charslots.filter(item => item['ifname'] == 'dragon_juicer');
+			newifpoints = cgslots.filter(item => item['ifname'] == 'dragon_juicer');
 			//console.log (cgslots);
 			//console.log (cgslots);
 			console.log (newifpoints);
