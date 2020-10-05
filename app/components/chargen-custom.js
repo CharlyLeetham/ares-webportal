b10952b55d323185f53e5ee8d2baa06ff3c344e0
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-04 15:02:42 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -39,7 +39,7 @@ export default Component.extend({
 			cgslots = this.get('char.custom.cgslots');
 			//console.log (cgslots);
 			for (const [key, value] of Object.entries(cgslots)) {
-				console.log ('Slot: ' +key - Value: '+value);
+				console.log ('Slot: ' +key '- Value: '+value);
 			}
 			// for (const [key, value] of Object.entries(charcgp)) {
 				//console.log(`Key: ${key}: ${value}`); 
