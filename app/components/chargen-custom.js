29908911b3f7105b0303b77ab92b43d64299478c
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-04 21:42:09 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -40,8 +40,7 @@ export default Component.extend({
 			cgslots = this.get('char.custom.cgslots');  // This is the cgslots at init and their values.
 			console.log ('CGslots: '+typeof cgslots);
 			//newifpoints = cgslots.filter(item => item['ifname'] == 'dragon_juicer');
-			newifpoints = Object.values(charcgp)
-			//.filter(slots => slots.ifname == 'dragon_juicer')
+			newifpoints = Object.values(charcgp).filter(slots => slots.ifname == 'dragon_juicer')
 			//console.log (cgslots);
 			//console.log (cgslots);
 			console.log (newifpoints);
