285f9fdc494fac3f07e0bb0ceec7b17aa53ef0b6
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-05 21:26:51 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -46,9 +46,9 @@ export default Component.extend({
 			for (const [key, value] of Object.entries(cgslots)) {
 				//console.log ("inp-" + value['name'] + "=" + value['value'])
 				resetifpoints = newifpoints.filter(slots => slots.name.toString() == value['name']);
-				console.log ('resetifpoints: '+resetifpoints);
+				//console.log ('resetifpoints: '+resetifpoints);
 				for (const [key1, value1] of Object.entries(resetifpoints)) {
-					console.log('Key1: '+key1+' Value: '+value1);
+					//console.log('Key1: '+key1+' Value: '+value1);
 					for (const [key2, value2] of Object.entries(value)) {
 						console.log('Key2: '+key2+' Value2: '+value2);
 					}
