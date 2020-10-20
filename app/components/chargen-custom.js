58592c01541d0155896fa3c9832fe8147d0b47fd
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-19 18:27:32 -1000

    d

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -6,8 +6,7 @@ export default Component.extend({
   
 	swiconicfsel: computed(function() {
 		var swiconicf;
-		swiconicf = this.get('char.custom.iconicf');
-		console.log(swiconicf);		
+		swiconicf = this.get('char.custom.iconicf');	
 		return swiconicf;
 	}), 
 	
