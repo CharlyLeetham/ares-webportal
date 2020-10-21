6aab6c62b4d149132f6862a57ddb4283f49eb368
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-20 23:46:24 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -146,7 +146,7 @@ export default Component.extend({
 			}					
 			
 			for (const [k, v] of Object.entries(evalrace)) {
-				var dislist44 = Object.values(swiconicfall).filter(slots => slots.class.toString().toLowerCase() == v); // Convert the iconic framework list to an array and filter for any entries that match the new framework selected.	
+				var dislist44 = Object.values(swiconicf).filter(slots => slots.class.toString().toLowerCase() == v); // Convert the iconic framework list to an array and filter for any entries that match the new framework selected.	
 							console.log ('here3');
 				for (const [k1, v1] of Object.entries(dislist44)) {
 					v1['disabled'] = true //Set disabled for this element to true							
