7b2966761c0aff1cfb965f91100534fa9459a868
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-24 21:12:04 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -149,6 +149,7 @@ export default Component.extend({
 			}
 		}
 		
+		console.log(evalrace);
 		dislist44 = Object.values(swrace).filter(slots => slots.disabled.toString().toLowerCase() == 'true'); // Convert the iconic framework list to an array and filter for any entries that match the new framework selected.
 				
 		
