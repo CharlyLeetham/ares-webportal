636148748f17d45f05f2d34b60ae1e2d87d60d33
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-25 20:57:57 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -44,7 +44,7 @@ export default Component.extend({
   },
  
 	changedges: function(sysedg, newedgarray, traittype, fw) {
-		var cgtr1=[], cgtr2=[], i, en, specchar, dislist, exedg, exhind;
+		var cgtr1=[], i, en, specchar, dislist, exedg, exhind;
 
 		exedg = this.get('char.custom.cgedges');		
 		exhind = this.get('char.custom.cghind');		
@@ -63,9 +63,9 @@ export default Component.extend({
 						console.log (desc);
 						var newdesc = desc['class'];
 						cgtr1[i] = [];
-						cgtr2[i]['class']=desc['class'];
-						cgtr2[i]['name']=desc['name'];
-						cgtr2[i]['rating']=desc['rating'];
+						cgtr1[i]['class']=desc['class'];
+						cgtr1[i]['name']=desc['name'];
+						cgtr1[i]['rating']=desc['rating'];
 						i=i+1;
 					}
 			}
