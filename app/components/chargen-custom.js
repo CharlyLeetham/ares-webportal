69ac80af1288a6554af5720c4db8bd9fe991d928
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-25 20:11:04 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -56,7 +56,9 @@ export default Component.extend({
 			exedg = this.get('char.custom.cgedges');
 			for (const[ed, desc] of Object.entries(exedg)) {
 				for (const[k1, v1] of Object.entries(desc)) {
-					console.log ('Ed: '+k1+' Desc: '+v1);
+					if (k1['class'].includes('*')) {
+						console.log ('Ed: '+k1+' Desc: '+v1);
+					}
 				}
 			}
 		}		
