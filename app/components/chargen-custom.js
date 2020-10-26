4ed2b893024ce6d150dcb236a54df21a61378e57
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-25 20:44:30 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -59,7 +59,7 @@ export default Component.extend({
 		if ( traittype=="edge" && fw=='icf' ) {
 			for (const[ed, desc] of Object.entries(exedg)) {
 					console.log (desc['class']);
-					if (desc['class'].includes('^')) {
+					if (desc['class'].includes('*')) {
 						cgtr1[i]['class']=desc['class'];
 						cgtr1[i]['name']=desc['name'];
 						cgtr1[i]['rating']=desc['rating'];
@@ -70,7 +70,7 @@ export default Component.extend({
 			for (const[ed, desc] of Object.entries(exedg)) {
 					console.log (desc);
 					if (desc['class'].includes('*')) {
-						cgtr1[i]['class']=desc['class'];
+						// cgtr1[i]['class']=desc['class'];
 						// cgtr1[i]['name']=desc['name'];
 						// cgtr1[i]['rating']=desc['rating'];
 						// i=i+1;
