e6125a23933a319b4cc336b04019cfc7d99e57fb
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-25 20:53:04 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -61,7 +61,8 @@ export default Component.extend({
 					// console.log (desc['class']);
 					if (desc['class'].includes('*')) {
 						console.log (desc);
-						cgtr2[i]['class']=desc['class'];
+						var newdesc = desc['class'];
+						cgtr2[i]['class']=newdesc;
 						cgtr2[i]['name']=desc['name'];
 						cgtr2[i]['rating']=desc['rating'];
 						i=i+1;
