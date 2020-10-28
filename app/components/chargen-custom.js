78071ca9d96c7d80fac102fb11835b4f10c7a854
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-27 20:57:52 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -120,7 +120,6 @@ export default Component.extend({
 				en = value1.split(specchar)[0].toLowerCase().trim(); // Take the trailing * from the edge for I/F's
 				dislist = Object.values(sysedg).filter(slots => slots.name.toString().toLowerCase() == en); // Convert the iconic framework list to an array and filter for any entries that match the new framework selected.
 
-				console.log(cgtr1);
 				for (const [key, value] of Object.entries(dislist)) {
 					value['disabled'] = true //Set disabled for this element to true
 					// Write the new CG Edges array for a nice display
@@ -132,6 +131,7 @@ export default Component.extend({
 					if ( dislist55.length > 0 ) { // If so, check to see if it's a IF Trait or Race Trait
 						for (var i = 0; i < dislist55.length; i++) {
 							var newclass = dislist55[i]['class'];
+							console.log (dislist55);
 							if ( fw=='icf' ) {
 								if (newclass.includes('^')) {
 									value['class'] = value['class'].replace("*", "*^");
