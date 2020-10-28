cef9456988aea36fae59bc8ceddc398991ec74a8
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-27 19:40:43 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -128,7 +128,7 @@ export default Component.extend({
 					
 					dislist55 = Object.values(cgtr1).filter(slots => slots.name.toString().toLowerCase() == en); // Convert the iconic framework list to an array and filter for any entries that match the new framework selected.
 					
-					if (dislist55) { // If so, check to see if it's a IF Trait or Race Trait
+					if ( dislist55.length > 0 ) { // If so, check to see if it's a IF Trait or Race Trait
 						console.log(dislist55);
 						// if ( fw=='icf' ) {
 							// if (dislist55['class'].includes('^')) {
