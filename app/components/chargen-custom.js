f2f820ca5a860de33acb3eb65349ff0ac5c76257
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-28 17:56:49 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -130,7 +130,6 @@ export default Component.extend({
 					dislist55 = Object.values(cgtr1).filter(slots => slots.name.toString().toLowerCase() == en); // Convert the iconic framework list to an array and filter for any entries that match the new framework selected.
 					
 					for (const [key2, value2] of Object.entries(cgtr1)) {
-						loc1='';
 						console.log(value2);
 						if (value2['name'].toLowerCase().startsWith(en)) {
 							console.log('YEAH MATCH: '+key2+" : "+value2['class']);
