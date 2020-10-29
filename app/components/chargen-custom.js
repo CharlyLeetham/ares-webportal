f4bb274e1e0cf2f4201544aadc1b56b86964941b
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-28 21:53:57 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -144,14 +144,14 @@ export default Component.extend({
 								loc1 = value2['class'].split('^')[0].trim(); // Take the trailing * from the edge for I/F's
 								console.log("Loc1a: "+loc1);
 								loc1 = loc1+'*^';
-								cgtr1[key2]['class']=loc1;
+								cgtr1.splice(key,1);
 								console.log("Loc1aa: "+loc1);
 							} else {
 								loc1 = value2['class'].split('*')[0].trim(); // Take the trailing * from the edge for I/F's
 								console.log("Loc1b: "+loc1);
 								loc1 = loc1+'*^';
 								console.log("Loc1bb: "+loc1);
-								cgtr1[key2]['class']=loc1;
+								cgtr1.splice(key,1);
 							}
 							
 							cgtr1[key]['class'] = loc1;
