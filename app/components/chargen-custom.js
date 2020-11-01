757bccd0169f0a60de741a34f0bedbdba4226ff0
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-31 17:31:24 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -557,7 +557,9 @@ export default Component.extend({
 			var charhind, sysedges, dislist;
 			sysedges = this.get('char.custom.sysedges');
 			dislist = Object.values(sysedges).filter(slots => slots.name.toString() == val[0]['name']); // Convert charcgp to an array and filter for any entries that match the new framework selected.
-			console.log (dislist);
+			for (const [key, value] of Object.entries(dislist)) {
+				value['disabled'] = false //Set disabled for this element to false
+			}			
 			dislist[0]['disabled'] = true; //Set disabled for this element to false
 			this.set('char.custom.cgedges', dislist); //Send the new array back to the page for nice display.
 			this.set('char.custom.cgedgesnofw', val);
