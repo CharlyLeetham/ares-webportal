15a0ce6e1e6902b7b0ef29c3efad9fedf3c7cdce
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-31 16:58:28 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -557,7 +557,7 @@ export default Component.extend({
 			var charhind, sysedges, dislist;
 			sysedges = this.get('char.custom.sysedges');
 			dislist = Object.values(sysedges).filter(slots => slots.name.toString() == val,name); // Convert charcgp to an array and filter for any entries that match the new framework selected.
-			console.log(val);
+			console.log(val.name);
 			console.log (dislist);			
 			this.set('char.custom.cgedgesnofw', val);
 		},		
