72566896c227ee6f633077f2df07888729911ec3
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-31 17:59:21 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -564,7 +564,7 @@ export default Component.extend({
 		},		
 		
 		hindChanged(val) {
-			var charhind;
+			var charhind, dislist;
 			charhind = this.get('char.custom.swsyshind');
 			for (const [key, value] of Object.entries(val)) {
 				dislist = Object.values(charhind).filter(slots => slots.name.toString() == value['name']); // Convert sysedges to an array and filter for any entries that match the new framework selected.
