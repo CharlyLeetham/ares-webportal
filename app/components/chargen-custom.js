003cba95454ef72818c782b74286427511a629ac
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-11-01 12:54:44 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -582,9 +582,9 @@ export default Component.extend({
 			var syshind, charhind, dislist, dislist33, trexcludes;
 			syshind = this.get('char.custom.swsyshind');
 			charhind = this.get('char.custom.cghind');
-			console.log (syshind);
-			console.log (charhind);
-			console.log (val);
+			// console.log (syshind);
+			// console.log (charhind);
+			// console.log (val);
 			for (const[k1, v1] of Object.entries(charhind)) {
 				if (!v1['class'].endsWith('*^') || !v1['class'].endsWith('*') || !v1['class'].endsWith('^') ) {
 					dislist33 = Object.values(charhind).filter(slots => slots.name.toString().toLowerCase() != v1['name'].toLowerCase());
