b23d1a26d616fdfa9b6a44fb19293b6a6ad87bb3
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-10-31 21:28:27 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -585,7 +585,7 @@ export default Component.extend({
 			charhind = this.get('char.custom.swsyshind');
 			console.log (val);
 			for (const [key, value] of Object.entries(val)) {
-				dislist = Object.values(charhind).filter(slots => slots.name.toString().toLowerCase() == value['name']); // Convert sysedges to an array and filter for any entries that match the new framework selected.
+				dislist = Object.values(charhind).filter(slots => slots.name.toString().toLowerCase() == value['name'].toLowerCase()); // Convert sysedges to an array and filter for any entries that match the new framework selected.
 				console.log(dislist);
 				dislist[0]['disabled'] = true;
 				// Check to see the Hinderance excludes others and mark them as disabled.
