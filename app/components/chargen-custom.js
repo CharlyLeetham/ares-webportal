dc0b019d5c65c24cbed5b36a272da51ca51d3581
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-11-01 21:48:27 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -462,10 +462,10 @@ export default Component.extend({
 			console.log(newracepoints);
 		}		
 		
-		// for (const [key, value] of Object.entries(cgslots)) { //Loop through the init values. This is our yardstick.
-			// newifpoints = Object.values(racecgp).filter(slots => slots.ifname.toString() == newval); // Convert charcgp to an array and filter for any entries that match the new framework selected.
+		for (const [key, value] of Object.entries(cgslots)) { //Loop through the init values. This is our yardstick.
+			newifpoints = Object.values(racecgp).filter(slots => slots.ifname.toString() == newval); // Convert charcgp to an array and filter for any entries that match the new framework selected.
 			
-			// console.log(newifpoints);
+			console.log(newifpoints);
 			// if (Object.keys(resetifpoints).length === 0) { // If it isn't, do this.
 				// newrating = value['rating'];  // Set the value we're going to send back to the web. This is going to equal CGINIT.
 			// } else {
@@ -475,7 +475,7 @@ export default Component.extend({
 				// }
 			// }
 			// document.getElementById("inp-" + value['class']).value = newrating;  //Set the counters on the website.		
-		// }		
+		}		
 		
 
 		
