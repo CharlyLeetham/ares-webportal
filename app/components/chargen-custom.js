cc69e3056a672c4238b51dcaebb3e3c9be21f304
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-11-01 21:18:49 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -446,15 +446,17 @@ export default Component.extend({
 	},
 	
 	resetcounter: function(fw, fwname) {
-		var charcgp, cgslots, newifpoints, resetifpoints, newrating, newval, newicf, newrace;
+		var charcgp, cgslots, newifpoints, resetifpoints, newrating, newval, newicf, newicfpoints, newracepoints, newrace;
 		charcgp = this.get('char.custom.inicgpoints');  // This is the array of all the if's and values
 		cgslots = this.get('char.custom.cgslots');  // This is the cgslots at init and their values.
 		newicf = this.get('char.custom.charicf');
-		console.log(newicf);
 		newrace = this.get('char.custom.charrace');
-		console.log(newrace);
 		
-		console.log(cgslots);
+		if (newrace['class'].toLowerCase() != 'none') {
+			newracepoints = Object.values(charcgp).filter(slots => slots.ifname.toString() == newrace['class']); // Convert charcgp to an array and filter for any entries that match the new framework selected.	
+			console.log(newracepoints);
+		}
+		
 		newifpoints = Object.values(charcgp).filter(slots => slots.ifname.toString() == newval); // Convert charcgp to an array and filter for any entries that match the new framework selected.	
 
 		for (const [key, value] of Object.entries(cgslots)) { //Loop through the init values. This is our yardstick.
