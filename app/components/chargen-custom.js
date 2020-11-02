637310fbda1cefa9423acd560d35ad81bc8b503d
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-11-01 20:43:14 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -446,6 +446,7 @@ export default Component.extend({
 	},
 	
 	resetcounter: function(fw) {
+		var charcgp, cgslots, newifpoints, resetifpoints, newrating;
 		charcgp = this.get('char.custom.inicgpoints');  // This is the array of all the if's and values
 		cgslots = this.get('char.custom.cgslots');  // This is the cgslots at init and their values.
 		
