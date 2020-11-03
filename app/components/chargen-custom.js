f81a8837dc7b682b7a58e4cfcacf6cc8dfe240bb
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-11-02 22:01:51 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -634,10 +634,9 @@ export default Component.extend({
 			sysedges = this.get('char.custom.sysedges');
 			charedges = this.get('char.custom.cgedges');
 			
-			for (const[k1, v1] of Object.entries(charedges)) {
-				console.log (v1['class']);				
+			for (const[k1, v1] of Object.entries(charedges)) {				
 				if (!v1['class'].endsWith('*^') && !v1['class'].endsWith('*') && !v1['class'].endsWith('^') ) {
-
+					console.logt(v1['class'];
 					dislist33 = Object.values(val).filter(slots => slots.name.toString().toLowerCase() == v1['name'].toLowerCase());
 					if (dislist33.length < 1) {
 						v1['disabled'] = false;
