4db6236ea11b3e495a7c83b6b4d62275482a8a25
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-11-09 21:02:23 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -731,7 +731,7 @@ export default Component.extend({
 			console.log (group);
 			console.log (val);
           if (val) {
-            this.set(`custom.char.hjtables.${group}`, val);		
+            //this.set(`custom.char.hjtables.${group}`, val);		
           } else {
 			this.set('custom.char.htables.${group}', '');
             this.set('custom.char.hjtables.${group}', '');
