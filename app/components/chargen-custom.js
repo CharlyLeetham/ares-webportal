e3bf9dba69192586dafc0589b355ab4e921c1a3c
Charly Leetham <helpdesk@askcharlyleetham.com>
2020-12-23 11:44:00 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -721,7 +721,7 @@ export default Component.extend({
 		
 	
         groupChanged(group, val) {
-			var hjtable, tmptable={}, hjslots, newhjtable={};
+			var hjtable, tmptable, hjslots, newhjtable={};
 			hjslots = this.get('char.custom.hjslots');
 			hjtable = this.get('char.custom.hjtables');
 			// console.log ('hjslots: ', hjslots);
@@ -729,18 +729,18 @@ export default Component.extend({
 			// console.log ('Val: ', val);
 			
 			for (const[k1, v1] of Object.entries(hjslots)) {
-				console.log('hjslots: K1: ',k1);
-				console.log ('V1: ', v1);		
-	
-				if (val) {
-					tmptable = Object.values(hjtable).filter(slots => slots.name.toString().toLowerCase() == val.name.toLowerCase()); // Convert sysedges to an array and filter for any entries that match the new framework selected.
-					if (tmptable) {
-							console.log ('tmptable: ',tmptable);
-							tmptable[k1]['table'] = val.table;
-					}
-					this.set ('char.custom.hjtables', hjtable);
-				  }
+				// console.log('hjslots: K1: ',k1);
+				// console.log ('V1: ', v1);
 			}
+		
+		if (val) {
+			tmptable = Object.values(hjtable).filter(slots => slots.name.toString().toLowerCase() == val.name.toLowerCase()); // Convert sysedges to an array and filter for any entries that match the new framework selected.
+			if (tmptable) {
+					console.log ('tmptable: ',tmptable);
+					tmptable[0]['table'] = val.table;
+			}
+			this.set ('char.custom.hjtables', hjtable);
+          }
 		},
 		
 	}
