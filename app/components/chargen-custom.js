ab9959116e1b3747510a36e4f054916afe085c6d
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-01-10 13:18:45 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -464,7 +464,7 @@ export default Component.extend({
 		
 		console.log(newicf['class'].toLowerCase());
 		
-		if (newicf['class'].toString().toLowerCase() != 'none') {
+		if (newicf['class'].toString() != 'none') {
 			
 			newicfpoints = Object.values(charcgp).filter(slots => slots.ifname.toString() == newicf['class'].toLowerCase()); // Convert charcgp to an array and filter for any entries that match the new framework selected.			
 			icfval = newicf['class'].toLowerCase();
