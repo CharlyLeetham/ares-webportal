e37c56c9f11d8567375914f71ce55b9127ae985a
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-01-10 13:25:21 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -473,7 +473,7 @@ export default Component.extend({
 		
 		if (newrace['class'] != 'none') {
 			console.log('here4');
-			newracepoints = Object.values(racecgp).filter(slots => slots.ifname.toString() == newrace['class'].toLowerCase()); // Convert charcgp to an array and filter for any entries that match the new framework selected.	
+			newracepoints = Object.values(racecgp).filter(slots => slots.ifname.toString() == newrace['class']); // Convert charcgp to an array and filter for any entries that match the new framework selected.	
 			raceval = newrace['class'].toLowerCase();
 		}
 		
