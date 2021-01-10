4806c5926838d41b26e32465bb2c4c9322914b51
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-01-10 12:48:34 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -518,7 +518,7 @@ export default Component.extend({
 
 
 			// If the None option is selected, reset the lists.
-			if (val.class.toLowerCase() == 'none') {
+			if (val.toLowerCase() == 'none') {
 				// Need to reset the ICF dropdown if this is the case.
 				this.fwreset(swrace, 'icf');
 				return;
