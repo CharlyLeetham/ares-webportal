9ad7447acf43a86230f1e63f7c4b51dee78316f3
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-01-10 12:49:52 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -517,6 +517,7 @@ export default Component.extend({
 			cghind = this.get('char.custom.cghind'); // Hinderances on the Character.
 
 
+			console.log(val);
 			// If the None option is selected, reset the lists.
 			if (val.toLowerCase() == 'none') {
 				// Need to reset the ICF dropdown if this is the case.
