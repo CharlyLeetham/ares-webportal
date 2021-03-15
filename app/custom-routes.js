47badf4bb1df4bfd61f5358527ddb763257a63e1
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-03-14 14:21:33 -1000

    Update custom-routes.js

--- app/custom-routes.js
+++ app/custom-routes.js
@@ -5,8 +5,5 @@ export default function setupCustomRoutes(router) {
   
   router.route('swriftsedges',  { path: '/edges'}); // Define the page to display all the SWRifts edges.
   router.route('swriftshinderances',  { path: '/hinderances'}); // Define the page to display all the SWRifts edges.
-  router.route('swriftsiconicf',  { path: '/icf'}); // Define the page to display all the SWRifts edges.
-  router.route('swriftsrace',  { path: '/races'}); // Define the page to display all the SWRifts edges.
-  router.route('swriftsskills',  { path: '/skills'}); // Define the page to display all the SWRifts edges.
-  router.route('swriftsppower',  { path: '/powers'}); // Define the page to display all the SWRifts edges.
+
 }
