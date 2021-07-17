e446fb7a5493d7263e253b5c7efa0aedc822e902
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-07-17 13:23:01 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -124,7 +124,8 @@ export default Component.extend({
 		}
 		
 		//At this point cgtr1[] should be a list of either ICF or race features only.
-	console.log('What have we here? ' + cgtr1);
+	console.log('What have we here? ');
+	console.log (cgtr1);
 		
 		// Clear the edges list for the framework
 
