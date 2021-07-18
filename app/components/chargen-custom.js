d487788e0a2587f9cd8e295d6b377c358456de4f
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-07-17 15:25:21 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -140,6 +140,7 @@ export default Component.extend({
 				console.log ('En: '+en);
 
 				dislist = Object.values(sysedg).filter(slots => slots.name.toString().toLowerCase() == en); // Convert the iconic framework list to an array and filter for any entries that match the new framework selected.
+				console.log (dislist);
 				for (const [key1, value1] of Object.entries(dislist)) {
 					value1['disabled'] = true; //Set disabled for this element to true
 					// Write the new CG Edges array for a nice display
