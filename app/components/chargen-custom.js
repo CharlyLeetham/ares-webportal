476224baa97f68400e848d4a2c9dc6dbaf05f47c
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-07-25 14:19:09 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -450,7 +450,8 @@ export default Component.extend({
 		i = 0;
 
 		console.log ( 'Edg: ' + Object.keys(exedg).length );		
-		console.log ( 'Edg Array: ' + exedg );		
+		console.log ( 'Edg Array: ' );
+		console.log ( exedg );		
 		console.log ( 'Hind: ' + Object.keys(exhind).length );		
 		
 		// Reset attributes for ICF set to None
