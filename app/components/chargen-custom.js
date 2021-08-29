7938a2ca09974885feae84b70801e70cae3ebb06
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-08-28 18:35:55 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -389,10 +389,10 @@ export default Component.extend({
 					if (value.edges) { //Complications exist on the character
 						for (const [k, v] of Object.entries(value.edges)) {
 							///// Debugging /////
-							// if ( traittype == 'race' ) {
-							 // console.log('Key: '+k);
-							 // console.log('Vlaue:' +v);
-							// }
+							if ( traittype == 'race' && v != null ) {
+							 console.log('Key: '+k);
+							 console.log('Vlaue:' +v);
+							}
 							///// End Debug /////
 							if (ppe_check == true) {
 								var ppetest = comptypearray.includes(v.toLowerCase());	
