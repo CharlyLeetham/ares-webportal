5c7ea3e6c0c746b9fa4aa08a81e0c5cf0de41ba9
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-08-28 19:42:02 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -191,7 +191,7 @@ export default Component.extend({
 		
 		//Get all the details for the New Edges passed in. This is need to do the combination of the arrays below.
 		
-		if ( newedgarray ) {
+		if ( newedgarray && newedgarray != null ) {
 			console.log ( newedgarray );
 			var ctr1=0;
 			for ( const [key, value] of Object.entries(newedgarray) ) {  // We need to find matching traits and apply the appropriate special characters to the end.
