21e05bc1970c1e7fe3b7fc26c92fe3ed1ea46c13
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-09-12 14:53:08 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -401,7 +401,7 @@ export default Component.extend({
 
 
 
-					if ( value.complications && value.complications[0].length > 0 ) {
+					if ( value.complications && (value.complications.length > 0 || value.complications[0].length > 0 ) {
 						// if ( traittype == 'race' ) {
 						 // console.log( 'Key: ' +key );
 						 console.log( 'Val: ' );
