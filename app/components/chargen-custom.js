4ba734e223ded747238bc9c48680a1aadc61d8a0
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-10 20:43:12 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -532,7 +532,6 @@ export default Component.extend({
 		if ( dragon_check ) {
 			dislist44 = Object.values( listsys );  // Convert the framework list to an array	
 			for (const [k1, v1] of Object.entries(dislist44)) {
-				console.log ('Dragons: ');
 				console.log ( dragonrace);
 				console.log ( 'Class: '+v1['class'] );
 				if ( dragonrace.includes( v1['class'] ) ) {
