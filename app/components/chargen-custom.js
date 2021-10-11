71abd03b20362bcc373370b6770c07bfb04c7977
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-10 20:02:12 -1000

    Update chargen-custom.js

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -540,7 +540,7 @@ export default Component.extend({
 		
 		if ( dragonrace ) {
 			for (const [k, v] of Object.entries(dragonrace)) {
-				dislist44 = Object.values( listsys ).filter(slots => slots.disabled.toString().toLowerCase() == 'false');  // Find all the entries and filter for disabled = false. 
+				dislist44 = Object.values( listsys ).filter(slots => slots.disabled.toString().toLowerCase() == 'true');  // Find all the entries and filter for disabled = true. 
 				dislist44 = Object.values( listsys ).filter(slots => slots.class.toString().toLowerCase() == v.toLowerCase()); // Convert the string to lower case to allow for better checking.
 				// Convert the iconic framework list to an array and filter for any entries that match the new framework selected.	
 				for (const [k1, v1] of Object.entries(dislist44)) {
