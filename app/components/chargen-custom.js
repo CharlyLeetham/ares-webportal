6be5d12e0b9926d7e2c7a9df692322033c2dce0d
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-24 19:47:03 -1000

    x

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -458,7 +458,7 @@ export default Component.extend({
         if ( (Array.isArray( value.complications ) && value.complications[0] !== null) ) {
           // Check to see if the complications array includes anything in the bp_check array
           for ( const [k, v] of Object.entries( value.complications ) ) {
-            var bp_check = rbp.includes(v.toLowerCase());
+            var bp_check = rbp.includes(v);
             if ( value.name == 'Juicer') {
               console.log ('Tis a Juicer!');
               console.log (v);
