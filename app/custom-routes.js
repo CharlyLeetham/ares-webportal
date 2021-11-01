45a42a617ebb8cd6732da19aee5fce18a30c99e2
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-31 15:07:18 -1000

    Adding Abilities listings to site

--- app/custom-routes.js
+++ app/custom-routes.js
@@ -2,10 +2,11 @@ export default function setupCustomRoutes(router) {
   // Define your own custom routes here, just as you would in router.js but using 'router' instead of 'this'.
   // For example:
   // router.route('yourroute');
-  
+
   router.route('swriftsedges',  { path: '/edges'}); // Define the page to display all the SWRifts edges.
   router.route('swriftshinderances',  { path: '/hinderances'}); // Define the page to display all the SWRifts edges.
-  router.route('swriftsiconicf',  { path: '/icf'}); // Define the page to display all the SWRifts edges.  
+  router.route('swriftsiconicf',  { path: '/icf'}); // Define the page to display all the SWRifts edges.
   router.route('swriftsrace',  { path: '/races'}); // Define the page to display all the SWRifts edges.
-  router.route('swriftsskills',  { path: '/skills'}); // Define the page to display all the SWRifts edges.  
+  router.route('swriftsskills',  { path: '/skills'}); // Define the page to display all the SWRifts edges.
+  router.route('swriftsabilities',  { path: '/abilities'}); // Define the page to display all the SWRifts edges.    
 }
