5190626becfab2ef87990be139bc905f8639467e
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-10-31 15:21:49 -1000

    d

--- app/routes/swriftsabilities.js
+++ app/routes/swriftsabilities.js
@@ -8,7 +8,7 @@ export default Route.extend(DefaultRoute, {
 
     model: function() {
         let api = this.gameApi;
-        return api.requestOne('abilities');
+        return api.requestOne('swabilities');
     },
 
     afterModel: function() {
