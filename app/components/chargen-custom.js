3ae29a4a3e898067dc2e277f84e7a854ecc53408
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-11-07 14:36:36 -1000

    dd

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -39,7 +39,7 @@ export default Component.extend({
     var newhjtables, sysiconicfall, charicf;
     sysiconicfall = this.get('char.custom.sysiconicf');
     charicf = this.get('char.custom.charicf');
-    console.log (swiconicfall);
+    console.log (sysiconicfall);
     console.log (charicf);
 
     //newhjtables = Object.values(swiconicfall).filter(slots => slots.name.toString().toLowerCase() == newval); // Convert swiconicfall to an array and filter for any entries that match the new framework selected.
