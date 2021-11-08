1baeffe50d7ef96928116db389a5d3590e7552dc
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-11-07 15:05:24 -1000

    r

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -48,22 +48,22 @@ export default Component.extend({
     newhjtables = newhjtables[0];
 
     console.log (newhjtables);
-    if (newhjtables) {
-      var tmptable=[], hjname, i;
-      for (const [key, value] of Object.entries(newhjtables)) {
-        if (key.startsWith('hj')) {
-          hjname = key.split('_')[0].toLowerCase().trim(); //Take the key name and remove the _ and everything after.
-          hjnumber = hjname.replace('hj','');
-          tmptable[hjname]=[];
-          for (const [k1, v1] of Object.entries(charicf)) {
-            tmptable[hjname]['tablenumber']=hjnumber;
-            tmptable[hjname]['details']=[];
-            tmptable[hjname]['details']['name']=hjname;
-            tmptable[hjname]['details']['table']=v1;
-          }
-        }
-      }
-    }
+    //if (newhjtables) {
+      //var tmptable=[], hjname, i;
+      //for (const [key, value] of Object.entries(newhjtables)) {
+        //if (key.startsWith('hj')) {
+        //  hjname = key.split('_')[0].toLowerCase().trim(); //Take the key name and remove the _ and everything after.
+        //  hjnumber = hjname.replace('hj','');
+        //  tmptable[hjname]=[];
+        //  for (const [k1, v1] of Object.entries(value)) {
+        //    tmptable[hjname]['tablenumber']=hjnumber;
+        //    tmptable[hjname]['details']=[];
+        //    tmptable[hjname]['details']['name']=hjname;
+        //    tmptable[hjname]['details']['table']=v1;
+        //  }
+      //  }
+    //  }
+  //  }
 
     //console.log (tmptable);
     //return (tmptable);
