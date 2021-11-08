af85d3349f4f94b7ef203c556ed4233ea4e3a4cb
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-11-07 15:03:33 -1000

    d

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -47,6 +47,7 @@ export default Component.extend({
     console.log (newhjtables);
     newhjtables = newhjtables[0];
 
+    console.log (newhjtables);
     if (newhjtables) {
       var tmptable=[], hjname, i;
       for (const [key, value] of Object.entries(newhjtables)) {
@@ -54,12 +55,12 @@ export default Component.extend({
           hjname = key.split('_')[0].toLowerCase().trim(); //Take the key name and remove the _ and everything after.
           hjnumber = hjname.replace('hj','');
           tmptable[hjname]=[];
-            for (const [k1, v1] of Object.entries(value)) {
-              tmptable[hjname]['tablenumber']=hjnumber;
-              tmptable[hjname]['details']=[];
-              tmptable[hjname]['details']['name']=hjname;
-              tmptable[hjname]['details']['table']=v1;
-            }
+          for (const [k1, v1] of Object.entries(charicf)) {
+            tmptable[hjname]['tablenumber']=hjnumber;
+            tmptable[hjname]['details']=[];
+            tmptable[hjname]['details']['name']=hjname;
+            tmptable[hjname]['details']['table']=v1;
+          }
         }
       }
     }
