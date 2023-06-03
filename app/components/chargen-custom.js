bbb2da8abbf99a37250ca1e12f007bdb2b68f02b
CharlyLeetham <helpdesk@askcharlyleetham.com>
2023-06-03 16:07:52 +1000

    d

--- app/components/chargen-custom.js
+++ app/components/chargen-custom.js
@@ -94,7 +94,7 @@ export default Component.extend({
     return (tmptable);
   }),
 
-  	computedcharhind: computed(function() {
+  	computedcharhind: computed('model.char.swrifts.charperkpoints' function() {
 		var swiconicf;
 		swiconicf = this.get('char.custom.charperkpoints');
 		return swiconicf;
